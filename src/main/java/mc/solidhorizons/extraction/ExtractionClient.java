19e497541d8ab1a3d7614c528b0eb18413f2e09b
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2024-07-25 23:49:07 +0200

    Initial commit

---
+++ src/main/java/mc/solidhorizons/extraction/ExtractionClient.java
@@ -0,0 +1,11 @@
+package mc.solidhorizons.extraction;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtractionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/mc/solidhorizons/extraction/ExtractionDataGenerator.java
@@ -0,0 +1,11 @@
+package mc.solidhorizons.extraction;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ExtractionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
